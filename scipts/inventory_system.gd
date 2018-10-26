6e1da5a98b70708ae70038276c0aa58cfc71b986
tarTG <tgerdi@web.de>
2018-10-26 16:30:11 +0200

    --

--- scipts/inventory_system.gd
+++ scipts/inventory_system.gd
@@ -12,7 +12,7 @@ func _ready():
 	pass
 
 func init():
-	$ScrollContainer/GridContainer.columns = inventory_columns
+	$GridContainer.columns = inventory_columns
 	pass
 
 func change_inventory_size(size):
@@ -25,7 +25,7 @@ func change_inventory_size(size):
 			item_list[i] =slot.instance()
 			item_list[i].slot_id = i
 			item_list[i].item_id = 0
-			$ScrollContainer/GridContainer.add_child(item_list[i])
+			$GridContainer.add_child(item_list[i])
 		inventory_size = size
 	else: # throw away unused items
 		var throw_items = []
@@ -33,7 +33,7 @@ func change_inventory_size(size):
 			if item_list[i].item_id != 0:
 				throw_items.append(item_list[i].item_id)
 			
-			$ScrollContainer/GridContainer.remove_child(item_list[i])
+			$GridContainer.remove_child(item_list[i])
 			item_list.remove(item_list[i].slot_id -1)
 		inventory_size = size
 		return throw_items
@@ -42,11 +42,12 @@ func load(items):
 	item_list.resize(items.size())
 	for i in range(items.size()):
 		item_list[i] = slot.instance()
-		var slot = item_list[items[i]["slot"]]
-		slot.slot_id = items[i]["slot"]
-		slot.item_id = items[i]["item_id"]
-		slot.count_num = items[i]["count"]
-		$ScrollContainer/GridContainer.add_child(item_list[i])
+		#var slot = item_list[items[i]["slot"]]
+		
+		$GridContainer.add_child(item_list[i])
+		item_list[items[i]["slot"]].slot_id = items[i]["slot"]
+		item_list[items[i]["slot"]].item_id = items[i]["item_id"]
+		item_list[items[i]["slot"]].count_num = items[i]["count"]
 
 func add_item(item_id):
 		
